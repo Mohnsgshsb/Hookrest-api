e74b82aacbd9128ec374a60363c061b3e4278f48
Mohnsgshsb <mohndalcot18@gmail.com>
2026-04-15 10:40:47 +0200

    Update yt.js

--- src/api/downloader/yt.js
+++ src/api/downloader/yt.js
@@ -1,43 +1,54 @@
 const axios = require('axios');
 
 module.exports = function (app) {
+
     const ytdown = {
 
         api: {
-            convert: "https://ssyt.rip/mates/en/convert"
+            download: "https://hub.ytconvert.org/api/download"
         },
 
         headers: {
             'User-Agent': 'Mozilla/5.0 (Linux; Android 15; 2409BRN2CY Build/AP3A.240905.015.A2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.7680.177 Mobile Safari/537.36',
-            'Accept': 'application/json, text/javascript, */*; q=0.01',
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'x-requested-with': 'XMLHttpRequest',
-            'origin': 'https://ssyt.rip',
-            'referer': 'https://ssyt.rip/ar/',
+            'Accept': 'application/json',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Content-Type': 'application/json',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua': '"Chromium";v="146", "Not-A.Brand";v="24", "Android WebView";v="146"',
+            'sec-ch-ua-mobile': '?1',
+            'origin': 'https://media.ytmp3.gg',
+            'x-requested-with': 'mark.via.gp',
+            'sec-fetch-site': 'cross-site',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-dest': 'empty',
+            'referer': 'https://media.ytmp3.gg/',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            'priority': 'u=1, i'
         },
 
-        fixedId: "ypwjF/ZPYN5NeeaZmdEUA/fBXBzDBSjjFoE7kvd6crwoqkvR+ytjU400/osXV7ME",
-
-        isUrl: str => {
+        isUrl: (str) => {
             try { new URL(str); return true; } catch { return false; }
         },
 
         request: async (url) => {
             const { data } = await axios.post(
-                `${ytdown.api.convert}?id=${encodeURIComponent(ytdown.fixedId)}`,
-                new URLSearchParams({
-                    'platform': 'youtube',
-                    'url': url, // ✅ المتغير الوحيد
-                    'title': 'يا عزووو😂🤷🏻‍♂️',
-                    'id': ytdown.fixedId,
-                    'ext': 'mp3',
-                    'note': '128k',
-                    'format': '140-drc'
-                }),
+                ytdown.api.download,
+                {
+                    url: url,
+                    os: 'android',
+                    output: {
+                        type: 'audio',
+                        format: 'mp3'
+                    },
+                    audio: {
+                        bitrate: '128k'
+                    }
+                },
                 {
                     headers: ytdown.headers
                 }
             );
+
             return data;
         },
 
@@ -55,7 +66,7 @@ module.exports = function (app) {
         })
     };
 
-    // 🔥 Endpoint MP3
+    // 🔥 Endpoint
     app.get('/api/mp', async (req, res) => {
         const { url } = req.query;
 
